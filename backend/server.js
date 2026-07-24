bf2013c2362cc8dc04529945c4eb1fe08a722de4
zohamalikdev <zohamalik.dev@gmail.com>
2026-07-24 22:23:47 +0500

    course route added in server

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,27 @@ const express = require("express");
 
 const app = express();
 
+// Home Route
 app.get("/", (req, res) => {
     res.send("Welcome to StudentHub!");
 });
 
+// Courses Route
+app.get("/courses", (req, res) => {
+    res.json([
+        {
+            id: 1,
+            name: "Web Development",
+            instructor: "Zoha"
+        },
+        {
+            id: 2,
+            name: "Database Systems",
+            instructor: "Ali"
+        }
+    ]);
+});
+
 app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+    console.log(" Server running on http://localhost:3000");
 });
