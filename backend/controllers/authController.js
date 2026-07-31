a4045e731e50ec3e0cdd18056c61f78ad882dcec
zohamalikdev <zohamalik.dev@gmail.com>
2026-07-31 18:52:09 +0500

    feat: implement user login with JWT authentication

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const pool = require("../db");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
 
@@ -44,6 +45,98 @@ exports.register = async (req, res) => {
   }
 };
 
-exports.login = (req, res) => {
-    res.send("Login Controller");
+exports.login = async (req, res) => {
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            message: "Email and Password are required."
+        });
+    }
+
+    try {
+         const existingUser = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+    }
+    catch (error) {
+        console.log(error);
+          res.status(500).json({
+            message: "Server Error"
+        });
+    }
+}
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check if email and password are provided
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "Email and Password are required."
+    });
+  }
+
+  try {
+    // Find user by email
+    const user = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    // Check if user exists
+    if (user.rows.length === 0) {
+      return res.status(400).json({
+        message: "Invalid email or password"
+      });
+    }
+
+    // Compare entered password with hashed password
+    const isMatch = await bcrypt.compare(
+      password,
+      user.rows[0].password
+    );
+
+    // If password is incorrect
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid email or password"
+      });
+    }
+
+    // Generate JWT Token
+    const token = jwt.sign(
+      {
+        id: user.rows[0].id,
+        email: user.rows[0].email,
+        role: user.rows[0].role
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h"
+      }
+    );
+
+    // Send response
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user.rows[0].id,
+        name: user.rows[0].name,
+        email: user.rows[0].email,
+        role: user.rows[0].role
+      }
+    });
+
+  } 
+  catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Server Error"
+    });
+  }
 };
